15e4618abd7d389a317095a38800747d68f8b1cf
MehrajRahman <rahmanmehraj627@gmail.com>
2023-05-17 02:52:30 +0600

    added

--- main.js
+++ main.js
@@ -1,38 +1,64 @@
-$(document).ready(function(){
-
-  $('.fa-bars').click(function(){
-    $(this).toggleClass('fa-times');
-    $('nav').toggleClass('nav-toggle');
+$(document).ready(function () {
+  $(".fa-bars").click(function () {
+    $(this).toggleClass("fa-times");
+    $("nav").toggleClass("nav-toggle");
   });
 
-  $(window).on('scroll load',function(){
-    $('.fa-bars').removeClass('fa-times');
-    $('nav').removeClass('nav-toggle');
+  $(window).on("scroll load", function () {
+    $(".fa-bars").removeClass("fa-times");
+    $("nav").removeClass("nav-toggle");
   });
 
-  $('.count').each(function() {
+  $(".count").each(function () {
     var $this = $(this),
-        countTo = $this.attr('data-count');
-    $({ countNum: $this.text()}).animate({
-      countNum: countTo
-    },
-    {
-      duration: 5000,
-      step: function() {
-        $this.text(Math.floor(this.countNum));
+      countTo = $this.attr("data-count");
+    $({ countNum: $this.text() }).animate(
+      {
+        countNum: countTo,
       },
-      complete: function() {
-        $this.text(this.countNum + '+');
+      {
+        duration: 5000,
+        step: function () {
+          $this.text(Math.floor(this.countNum));
+        },
+        complete: function () {
+          $this.text(this.countNum + "+");
+        },
       }
-    });
+    );
   });
 
-  $('.project').magnificPopup({
-    delegate:'a',
-    type:'image',
-    gallery:{
-      enabled:true
-    }
+  $(".project").magnificPopup({
+    delegate: "a",
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
   });
 
+  $(".customer-logos").slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+    ],
+  });
 });
+
+// $(document).ready(function () {});
